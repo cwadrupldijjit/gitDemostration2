5949d61d321ee3a9d5a02631a8322b26b33eb289
Samuel Skeen <sskeen@sskeendevelopment.onmicrosoft.com>
2017-10-28 09:40:47 -0600

    Add Jared (so he doesn't feel left out)

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 
 import Josh from './josh'
+import Jared from './jared'
 
  class App extends Component {
   render() {
     return (
       <div>
         <h1>I ❤️ trailing commas</h1>
-        <Josh/>
+        <Jared/>
         <Josh/>
         <Josh/>
         <Josh/>
